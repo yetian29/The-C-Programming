7e2d37c82e253c887383bae08c258cafdea89645
yetian29 <dathien000@gmail.com>
2025-06-30 15:18:12 +0000

    Initial

---
+++ chapter1/exercise1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main() {
+    printf("hello, world\t"); // backslash t for tab between current line and next line
+    printf("hello, world\b"); // backslash b for backspace
+    printf("hello, world\""); // backslash " add quote end word
+    return 0;
+}
