5e94644fd5dcaa7872e1117e6d5fcd915ccd66f4
yetian29 <dathien000@gmail.com>
2025-07-01 03:21:13 +0000

    Update

--- chapter1/exercise1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-
-int main() {
-    printf("hello, world\t"); // backslash t for tab between current line and next line
-    printf("hello, world\b"); // backslash b for backspace
-    printf("hello, world\""); // backslash " add quote end word
-    return 0;
-}
---
+++ chapter1/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main() {
+    // printf("hello, world\t"); // backslash t for tab between current line and next line
+    // printf("hello, world\b"); // backslash b for backspace
+    // printf("hello, world\""); // backslash " add quote end word
+    // printf("hello, world\\"); // backslash \ add backslash end word
+    printf("hello, world\n"); // bachslash n for a newline character
+
+    return 0;
+}
