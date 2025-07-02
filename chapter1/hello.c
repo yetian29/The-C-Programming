10ee63cba28e1a83dcb75e5804f0d2130b6e017a
yetian29 <dathien000@gmail.com>
2025-07-02 01:13:29 +0000

    Update

--- chapter1/hello.c
+++ chapter1/hello.c
@@ -6,7 +6,8 @@ int main() {
     // printf("hello, world\b"); // backslash b for backspace
     // printf("hello, world\""); // backslash " add quote end word
     // printf("hello, world\\"); // backslash \ add backslash end word
-    printf("hello, world\n"); // bachslash n for a newline character
+    // printf("hello, world\n"); // bachslash n for a newline character
+    // printf("hello, wolrd\c");
 
     return 0;
 }
