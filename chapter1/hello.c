5faaa3fd352545869299ce640a1e0c4c4b82a30b
yetian29 <dathien000@gmail.com>
2025-07-02 01:21:22 +0000

    Update

--- chapter1/hello.c
+++ chapter1/hello.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-
 int main() {
     // printf("hello, world\t"); // backslash t for tab between current line and next line
     // printf("hello, world\b"); // backslash b for backspace
     // printf("hello, world\""); // backslash " add quote end word
     // printf("hello, world\\"); // backslash \ add backslash end word
     // printf("hello, world\n"); // bachslash n for a newline character
-    // printf("hello, wolrd\c");
+    printf("hello, world\c"); // display warning unknown escape sequence backslash c
 
     return 0;
 }
