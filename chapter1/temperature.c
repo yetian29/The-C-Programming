0811a75a01de453d2ca7c5fd4ed8298cdfd11363
yetian29 <dathien000@gmail.com>
2025-07-03 07:25:17 +0000

    Update

--- chapter1/temperature.c
+++ chapter1/temperature.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
-
 int main() {
     /*print fahrenheit-celsious table
     for fahrenhrit = 0, 20, ..., 300
     */
-    int celsius, fahrenheit;
-    int lower, upper, step;
+    // int celsius, fahrenheit;
+    float celsius;
+    int fahrenheit, lower, upper, step;
     lower = 0;
     upper = 300;
     step = 20;
     fahrenheit = lower;
     printf("fahrenheit\tcelsius\n");
     while (fahrenheit <= upper) {
-        celsius = 5 * (fahrenheit - 32) / 9;
-        printf("%3d\t\t%3d\n", fahrenheit, celsius);
+        celsius = 5.0 * (fahrenheit - 32) / 9;
+        // printf("%3d\t\t%3d\n", fahrenheit, celsius);
+        printf("%3d\t\t%6.1f\n", fahrenheit, celsius);
         fahrenheit = fahrenheit + step;
     }
     return 0;
 }
+
+
