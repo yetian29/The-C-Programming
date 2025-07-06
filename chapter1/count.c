9cfab86e7bb330a259384f46cc4751928c87cb63
yetian29 <dathien000@gmail.com>
2025-07-06 06:40:29 +0000

    Update

--- chapter1/count.c
+++ chapter1/count.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int nc;
-    for (nc = 0; getchar() != EOF; ++nc) {
+    // int nc;
+    // for (nc = 0; getchar() != EOF; ++nc) {
         ; // null statement
+    // }
+    int nc = 0;
+    while (getchar() != EOF) {
+        ++nc;
     }
     printf("Number characters: %d\n", nc);
     return 0;
