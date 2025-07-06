90ddb5074745e650e303847f318cf321fedb6dfd
yetian29 <dathien000@gmail.com>
2025-07-06 07:00:04 +0000

    Update

--- chapter1/count.c
+++ chapter1/count.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
 int main() {
+    /* Character counting */
     // int nc;
     // for (nc = 0; getchar() != EOF; ++nc) {
         ; // null statement
     // }
-    int nc = 0;
-    while (getchar() != EOF) {
-        ++nc;
+    // int nc = 0;
+    // while (getchar() != EOF) {
+        // ++nc;
+    // }
+    //printf("Number character: %d\n", nc);
+
+    /*Line counting*/
+    int nl = 0;
+    while ((c = gechar()) != EOF) {
+        if (c == "\n") {
+            ++nl;
+        }
     }
-    printf("Number characters: %d\n", nc);
+    printf("Number line: %d\n", nl);
     return 0;
 }
