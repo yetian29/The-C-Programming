942b438fec938d6d0c9d546087498e7856300881
yetian29 <dathien000@gmail.com>
2025-07-07 17:22:04 +0700

    Update

--- chapter1/copy.c
+++ chapter1/copy.c
@@ -3,6 +3,7 @@ Write a program to copy its input to its output,
 replacing each string of one or 
 more blanks by a single blank 
 */
+/*
 #include <stdio.h>
 int main() {
     int c, nb;
@@ -19,3 +20,25 @@ int main() {
     
     return 0;
  }
+*/
+/*
+Write a program to copy its input to its output,
+replacing each tab by \t, 
+each backspace by \b, 
+and each backslash by \\. 
+This makes tabs and backspaces visible in an unambiguous way.
+*/
+#include <stdio.h>
+int main() {
+    int c;
+    while ((c = getchar() != EOF) {
+        if (c == '\t')
+            c = \t;
+        else if (c == '\b')
+            c = \b;
+        else if (c == '\\')
+            c == \\;
+        putchar(c);
+    }
+    return 0;    
+}
