7962004d9427e85ad189fc478c9f453cc255302e
yetian29 <dathien000@gmail.com>
2025-07-07 15:39:05 +0700

    Add

---
+++ chapter1/copy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int c, nb;
+    nb = 0;
+    while (c = getchar() != EOF) {
+        if (c == ' ')
+            ++nb;
+        else 
+            nb = 0;
+        if (nb > 1)
+            c = '\0';
+        putchar(c);
+    }
+    
+    return 0;
+ }
