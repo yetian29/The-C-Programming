565f6c2ce866602e39967592789ed58f76657eff
yetian29 <dathien000@gmail.com>
2025-07-07 17:25:48 +0700

    Update

--- chapter1/copy.c
+++ chapter1/copy.c
@@ -33,11 +33,11 @@ int main() {
     int c;
     while ((c = getchar()) != EOF) {
         if (c == '\t')
-            c = \t;
+            c = '\t';
         else if (c == '\b')
-            c = \b;
+            c = '\b';
         else if (c == '\\')
-            c = \\;
+            c = '\\';
         putchar(c);
     }
     return 0;    
