9633aa67247e348786e93f1bf791411720093e66
yetian29 <dathien000@gmail.com>
2025-07-07 17:10:16 +0700

    Update

--- chapter1/copy.c
+++ chapter1/copy.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 /*
 Write a program to copy its input to its output, 
 replacing each string of one or 
 more blanks by a single blank 
 */
+#include <stdio.h>
 int main() {
     int c, nb;
     nb = 0;
