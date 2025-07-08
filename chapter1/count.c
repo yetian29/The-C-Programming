6948577e43605707b52f437ede52346f3ba01ee1
yetian29 <dathien000@gmail.com>
2025-07-08 10:14:04 +0700

    Update

--- chapter1/count.c
+++ chapter1/count.c
@@ -1,8 +1,12 @@
 /*Write a program to count characters, blanks, tabs, newlines and word.*/
 #include <stdio.h>
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
+
 int main() {
     int c, nc, nb, nt, nl, nw;
     nc = nb = nt = nl = nw =  0;
+    state = OUT;
     while ((c = getchar()) != EOF) {
         ++nc;   
         if (c == ' ')
