d3cfa28e62cfd1be998e7bb95c7b46c60f082b06
yetian29 <dathien000@gmail.com>
2025-07-08 09:52:23 +0700

    Update

--- chapter1/count.c
+++ chapter1/count.c
@@ -1,9 +1,10 @@
-/*Write a program to count blanks, tabs, and newlines.*/
+/*Write a program to count characters, blanks, tabs, newlines and word.*/
 #include <stdio.h>
 int main() {
-    int c, nb, nt, nl;
-    nb = nt = nl = 0;
-    while (c = getchar() != EOF) {
+    int c, nc, nb, nt, nl, nw;
+    nc = nb = nt = nl = nw =  0;
+    while ((c = getchar()) != EOF) {
+        ++nc;   
         if (c == ' ')
             ++nb;
         else if (c == '\t')
@@ -11,6 +12,6 @@ int main() {
         else if (c == '\n')
             ++nl;
     }
-    printf("Number blanks: %d\nNumber tabs:%d\nNumber lines:%d\n", nb, nt, nl);
+    printf("Number characters: %d\nNumber blanks: %d\nNumber tabs:%d\nNumber lines:%d\n",nc, nb, nt, nl);
     return 0;
 }
