aefc26b1ae7e037e4148d5d642fc8198cd74704c
yetian29 <dathien000@gmail.com>
2025-07-08 16:01:20 +0700

    Update

--- chapter1/print.c
+++ chapter1/print.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#define OUT 0 /* outside word */
+#define IN 1 /* inside word */
 
+int main() {
+  int c, state;
+  state = OUT;
+  while ((c = getchar()) != EOF) {
+    if (c == ' ' || c == '\t' || c == '\n')
+        state = OUT;
+    else { 
+        putchar(c);
+        if (state == OUT)
+            printf('\n');
+        state = IN;
+    }       
+    
+  }
+  return 0;
+}
