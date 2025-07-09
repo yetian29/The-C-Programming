dfcea49dce448702c6b06fc59f6b988ddc098ca1
yetian29 <dathien000@gmail.com>
2025-07-09 10:44:35 +0700

    Update

--- chapter1/print.c
+++ chapter1/print.c
@@ -1,4 +1,5 @@
 /* Write a program that prints its input one word per line. */
+/*
 #include <stdio.h>
 #define OUT 0 /* outside word */
 #define IN 1 /* inside word */
@@ -19,3 +20,9 @@ int main() {
   }
   return 0;
 }
+*/
+/*
+Write a program to print a histogram of the lengths of words in its input.
+It is easy to draw the histogram with the bars horizontal; 
+a vertical orientation is more challenging.
+*/
