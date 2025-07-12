bd8245cc6392edb9677cb925c41c62f2c1474016
yetian29 <dathien000@gmail.com>
2025-07-12 18:05:20 +0700

    Update

--- chapter1/power.c
+++ chapter1/power.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int power(int base, int exponent);
+int main() 
+    int i; 
+    for (i = 0; i < 10; ++i) 
+        printf("%d %d\n", power(2, i), power(-3, i));
+    return 0;
+}
+
+int power(int base, int exponent) {
+  int n, p;
+  p = 1;
+  for (n = 0; n <= exponent; ++n)
+      p = p * base;
+  return p;
+}
